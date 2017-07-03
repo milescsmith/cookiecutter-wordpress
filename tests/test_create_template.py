77beafb58e47d64777402951aab0030193150fb2
Green Guava Labs <team@greenguavalabs.com>
2017-07-03 16:35:13 -0700

    proj: Add composer.json

--- tests/test_create_template.py
+++ tests/test_create_template.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+import json
 
 
-class TestCreateTemplate:
+class SetupTemplate:
     @pytest.fixture()
-    def template(self, cookies):
-        return cookies.bake()
+    def context(self):
+        return None
 
+    @pytest.fixture()
+    def template(self, cookies, context):
+        if context is None:
+            return cookies.bake()
+        else:
+            return cookies.bake(extra_context=context)
+
+
+class TestCreateTemplate(SetupTemplate):
     def test_exit_code_is_zero(self, template):
         assert template.exit_code == 0
 
@@ -19,3 +29,46 @@ class TestCreateTemplate:
 
     def test_project_name_is_correct(self, template):
         assert template.project.basename == "wordpress"
+
+    files = (
+        ("composer.json"),
+    )
+
+    @pytest.mark.parametrize("file", files)
+    def test_file_was_created(self, template, file):
+        top_level_files = [f.basename for f in template.project.listdir()]
+        assert file in top_level_files
+
+
+class TestComposerJson(SetupTemplate):
+    @pytest.fixture
+    def context(self):
+        return {
+            "project_name": "wordpress",
+            "version": "0.1.0",
+            "wordpress_version": "4.8",
+        }
+
+    @pytest.fixture
+    def file(self, template):
+        return template.project.join("composer.json").readlines()
+
+    @pytest.fixture
+    def parsed_file(self, file):
+        return json.loads("".join(file))
+
+    def test_not_empty(self, file):
+        assert len("".join(file).strip()) is not 0
+
+    def test_is_valid_json(self, parsed_file):
+        assert parsed_file is not None
+
+    def test_has_project_name(self, context, parsed_file):
+        assert parsed_file["name"] == context["project_name"]
+
+    def test_has_version(self, context, parsed_file):
+        assert parsed_file["version"] == context["version"]
+
+    def test_has_wordpress_version(self, context, parsed_file):
+        assert parsed_file["require"]["johnpbloch/wordpress"] == \
+               context["wordpress_version"]
